34e3baef9730f0e15496d9b8a69b910bf4a7fe1d
fimojoha <fimojoha@localhost>
2011-12-24 15:45:23 +0100

    Slides 4 and 5

--- src/slide04.py
+++ src/slide04.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
 
+# En enkel funksjon
 def hello(name):
     print "Hello %s" % name
 
 hello("World")
 
+# En funksjon med varargs
 def hello_all(*names):
     print "Hello %s" % ", ".join(names)
 
 hello_all("Graham", "John", "Terry", "Eric", "Terry", "Michael")
-
-def hello_prefix(name, prefix="Hello"):
-    print "%s %s" % (prefix, name)
-
-hello_prefix("World")
-hello_prefix("Verden", "Hallo")
