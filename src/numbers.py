f860347b59c544e0c3518ce12ab0c2e62d401546
Morten Lied Johansen <mortenjo@ifi.uio.no>
2011-12-25 21:38:50 +0100

    Cleaned up numbers slide to reduce "overload"

--- src/numbers.py
+++ src/numbers.py
@@ -2,17 +2,13 @@
 # -*- coding: utf-8
 
 # Enkel addisjon av heltall
-print "2 + 2 =", 2+2
+print 2 + 2
 
 # Heltall og flyttall blir flyttall
-print "2 + 3.3 =", 2+3.3
-print "7 / 2.0 =", 7/2.0
+print 7 / 2.0
 
 # Ingen grense for hvor store tall man kan jobbe med
-print "12345678901234567891234567891234567 + 34567890123456789123456789123456789 =",
-print 12345678901234567891234567891234567+34567890123456789123456789123456789
+print 12345678901234567891234567891234567 + 34567890123456789123456789123456789
 
 # Innebygd støtte for imaginære tall
-print "3 + 1j * 3 =", 3+1j*3
-print "(3+1j) * 3=", (3+1j)*3
-print "(1+2j) / (1+1j)=", (1+2j)/(1+1j)
+print (3+1j) * 3
