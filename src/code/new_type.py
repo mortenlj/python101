16fb3ca348b4753a1e38fae6518bef46f8a4242c
Morten Lied Johansen <mortenjo@ifi.uio.no>
2012-01-08 15:55:33 +0100

    Wrap lines to avoid problems with long lines in low res situations.

--- src/code/new_type.py
+++ src/code/new_type.py
@@ -4,7 +4,11 @@
 def make_type(type_name, attributes):
     return type(type_name, (object,), attributes)
 
-tags = [("b", {}), ("img", {"src": "http://url.com/img.jpg", "alt": "An image"})]
+tags = [
+    ("b", {}),
+    ("img", {"src": "http://url.com/img.jpg", "alt": "An image"})
+]
 for name, attributes in tags:
     t = make_type(name, attributes)
-    print t, ["%s: %s" % (key, getattr(t, key)) for key in t.__dict__ if not key.startswith("__")]
+    print t, ["%s: %s" % (key, getattr(t, key))
+                    for key in t.__dict__ if not key.startswith("__")]
