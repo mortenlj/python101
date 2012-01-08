20127a71e8955cc3a30f06c35d825b9f38566a03
Morten Lied Johansen <mortenjo@ifi.uio.no>
2012-01-08 22:53:16 +0100

    Make hello world example a bit more interesting with unicode examples

--- src/code/hello.py
+++ src/code/hello.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
 
-print "Hello World"
+print "D'ḟuascail Íosa Úrṁac na hÓiġe Beannaiṫe pór Éava agus Áḋaiṁ."
+print "Blåbærsyltetøy"
+print "ξεσκεπάζω την ψυχοφθόρα βδελυγμία"
+print "Съешь же ещё этих мягких французских булок да выпей чаю."
+print "זה כיף סתם לשמוע איך תנצח קרפד עץ טוב בגן."
+
+# Samples collected from http://www.columbia.edu/~fdc/utf8/
