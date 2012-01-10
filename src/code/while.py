1a25139a332ad8b59d94c127dbc4218d04d82db0
Morten Lied Johansen <mortenjo@ifi.uio.no>
2012-01-10 22:06:06 +0100

    Cleaned up while-slide

--- src/code/while.py
+++ src/code/while.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
 
-starting_seconds = 2151
-seconds = starting_seconds
+seconds = starting_seconds = 2151
 minutes = 0
+
 while seconds > 60:
     minutes += 1
     seconds -= 60
+
 print "%d seconds = %d minutes and %d seconds" %\
                     (starting_seconds, minutes, seconds)
