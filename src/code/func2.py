80a8e3046aa3a6aab07960546d918591f86390f2
Morten Lied Johansen <mortenjo@ifi.uio.no>
2012-01-10 22:10:37 +0100

    Some explanation

--- src/code/func2.py
+++ src/code/func2.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8
 
 # A function with an optional paramter
-def hello(prefix, name="World"):
-    print "%s %s" % (prefix, name)
+def hello(salutation, name="World"):
+    print "%s %s" % (salutation, name)
 
 hello("Hello") # no second parameter, uses default value World
 hello("Hi", "There")
 
-# A function with varargs and keyword args
+# A function with varargs and variable keyword args
 def many(*args, **kwargs):
     print "args: %r, kwargs: %r" % (args, kwargs)
 
