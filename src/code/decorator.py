2dda89c0111bdf2dcd4e6c280550c4c8c0edba26
Morten Lied Johansen <mortenjo@ifi.uio.no>
2012-01-10 21:50:58 +0100

    Simplify the decorator slide

--- src/code/decorator.py
+++ src/code/decorator.py
@@ -7,12 +7,8 @@ def print_args(function):
         return function(*args, **kwargs)
     return wrapper
 
+@print_args # "apply" the "decorator" print_args to this function
 def yell(text):
     print text.upper()
-yell = print_args(yell)
-yell("foo")
 
-@print_args
-def write(text):
-    print text
-write("bar")
+yell("foo")
